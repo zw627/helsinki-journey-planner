e00e4c61ef0e925bd22795f42c407b4bdef0e152
zw67 <zhuanyan.wang@outlook.com>
2020-09-12 21:52:35 +0300

    Fix yet another path

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ if (!(app.get("env") === "development")) {
 app.use(express.static(path.join(__dirname, "/../client/build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/../client/build/index.html"));
 });
 
 // Use routes
